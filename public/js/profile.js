5b9c2683a3e02fd75c3f175b913bcf6e164fd050
crumwj22 <crumwj22@hotmail.com>
2022-06-04 11:07:11 -0700

    fixed route profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -15,7 +15,7 @@ const newFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/profile');
     } else {
       alert('Failed to create blog post');
     }
@@ -31,7 +31,7 @@ const delButtonHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/profile');
     } else {
       alert('Failed to delete blog post');
     }
