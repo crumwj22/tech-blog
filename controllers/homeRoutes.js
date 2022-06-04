4e84535a60a1c34d8503980c3dc63a15f337fba4
crumwj22 <crumwj22@hotmail.com>
2022-06-04 09:54:02 -0700

    fixed login route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,6 +49,16 @@ router.get('/blog/:id', async (req, res) => {
       },
     });
 
+    router.get('/login', (req, res) => {
+      // If the user is already logged in, redirect the request to another route
+      if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+      }
+
+      res.render('login');
+    });
+
     const blogData = await blogDataDb.get({ plain: true });
     const commentData = await commentDataDb.map((comment) =>
       comment.get({ plain: ture })
